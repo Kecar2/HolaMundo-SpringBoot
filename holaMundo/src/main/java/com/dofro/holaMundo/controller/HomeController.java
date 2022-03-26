a77a0a6b4354cde1631147d5aa25c7f700100bae
Ken <99403056+Kecar2@users.noreply.github.com>
2022-03-26 13:49:55 +0100

    Add files via upload

---
+++ holaMundo/src/main/java/com/dofro/holaMundo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.dofro.holaMundo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String inicio() {
+		return "Hola Mundo";	
+		}
+	
+}
